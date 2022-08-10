7e126fe6a4e46d63da3a3d3c1926e44ad165c3de
prash <87913082+codewithprash@users.noreply.github.com>
2022-08-10 16:26:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,222 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'POSTit',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+// text fields' controllers
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _postController = TextEditingController();
+
+  final CollectionReference _post =
+      FirebaseFirestore.instance.collection('post');
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  controller: _postController,
+                  decoration: const InputDecoration(
+                    labelText: 'Post',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String name = _nameController.text;
+                    final String post = _postController.text;
+
+                    await _post.add({"title": name, "text": post});
+
+                    _nameController.text = '';
+                    _postController.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      _nameController.text = documentSnapshot['title'];
+      _postController.text = documentSnapshot['text'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  controller: _postController,
+                  decoration: const InputDecoration(
+                    labelText: 'Post',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Update'),
+                  onPressed: () async {
+                    final String name = _nameController.text;
+                    final String post = _postController.text;
+
+                    await _post
+                        .doc(documentSnapshot!.id)
+                        .update({"title": name, "text": post});
+                    _nameController.text = '';
+                    _postController.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete(String productId) async {
+    await _post.doc(productId).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        backgroundColor: Colors.red,
+        content: Text('You have successfully deleted a POST')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('POSTit')),
+        ),
+        body: StreamBuilder(
+          stream: _post.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(documentSnapshot['title']),
+                      subtitle: Text(documentSnapshot['text']),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.blue,
+                                ),
+                                tooltip: 'Edit Post',
+                                onPressed: () => _update(documentSnapshot)),
+                            IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                                tooltip: 'Detete Post',
+                                onPressed: () => _delete(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new post
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          tooltip: 'New Post',
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+  }
+}
